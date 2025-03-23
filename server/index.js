980b00b12664a98aaa4f6a21ac7a64aa0e8be361
ksrevelar <karl.revelar@gmail.com>
2025-03-23 20:45:10 +0800

    First Commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const app = express();
@@ -50,24 +49,6 @@ app.post('/api/deep-search', async (req, res) => {
     const { query } = req.body;
     const results = `Searched medical data for "${query}": [Sample result]`;
     res.json({ results });
-});
-
-app.get('/stripe/checkout', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-            price_data: {
-                currency: 'usd',
-                product_data: { name: 'Token Subscription' },
-                unit_amount: 1000,
-            },
-            quantity: 1,
-        }],
-        mode: 'payment',
-        success_url: 'http://localhost:3000/success.html',
-        cancel_url: 'http://localhost:3000/cancel.html',
-    });
-    res.redirect(303, session.url);
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
